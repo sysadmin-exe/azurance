b8421ddf9e9bfbca23564b1b9192d4ea0196c21f
sysadmin-exe <victorefedi@gmail.com>
2025-04-05 16:30:54 +0200

    Added Examples

--- example/customer2/1_dev/main.tf
+++ example/customer2/1_dev/main.tf
@@ -1,15 +1,15 @@
 provider "azurerm" {
   features {}
-  
+
 }
 
 module "customer2_dev_eastus" {
   source = "https://github.com/sysadmin-exe/azurance.gitref=v0.0.1"
-  
+
   # Required variables
-  location             = "East US"
-  customer_name = "customer1"
-  environment          = "dev"
-  product_version       = "1.0.0"
-  platform_type        = "S"
+  location        = "East US"
+  customer_name   = "customer1"
+  environment     = "dev"
+  product_version = "1.0.0"
+  platform_type   = "S"
 }
